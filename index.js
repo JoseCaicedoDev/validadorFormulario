e02681189f538cd395d8c489615a2e90e62e094d
jcaicedo <jose.caicedo@outlook.com>
2022-08-27 23:58:39 -0500

    [update] validaciones

--- index.js
+++ index.js
@@ -10,19 +10,22 @@ const inputs = document.querySelectorAll('#form input');
 const campos ={
   name: false,
   email: false,
-  pass: false
+  pass: false,
 }
 
 const validateForm = (e) =>{
   switch(e.target.name){
     case 'nombre':
       validateCamp(expresiones.nombre, e.target, "name")
+      console.log(campos)
       break
       case 'email':
       validateCamp(expresiones.correo, e.target, "email")
+      console.log(campos)
       break
       case 'pass':
       validateCamp(expresiones.password, e.target, "pass")
+      console.log(campos)
       break
   }
 }
@@ -38,24 +41,42 @@ const validateCamp = (expressions, input, camp ) =>{
     campos[camp] = false
   }
 }
+
 inputs.forEach((input)=>{
   input.addEventListener('keyup', validateForm)
   input.addEventListener('blur', validateForm)
 })
+console.log(campos)
+
+const terminos = document.querySelector('#check')
+terminos.addEventListener('change', (e)=>{
+  if (campos.name && campos.email && campos.pass && terminos.checked){
+    //document.querySelector('input[type="submit"]').classList.remove('disableBtn')
+    document.querySelector('.messageError').classList.remove('active')
+  }else{
+    document.querySelector('input[type="submit"]').classList.add('disableBtn')
+  }
+})
 
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
-
-  const terminos = document.querySelector('#check')
-  if(campos.name && campos.email && campos.pass && terminos.checked){
+  console.log(validate())
+/*   if(campos.name && campos.email && campos.pass && terminos.checked){
     form.reset()
     document.querySelector('.message').classList.add('active')
-    document.querySelector('.messageError').classList.remove('active')
-    //document.querySelector('input[type="submit"]').classList.remove('disableBtn')
+    document.querySelector('input[type="submit"]').classList.remove('disableBtn')
     setTimeout(()=>{
       document.querySelector('.message').classList.remove('active')
-    },3000)
+    },2000)
   }else{
     document.querySelector('.messageError').classList.add('active')
-  }
+  } */
 })
+
+const validate = () => {
+  return campos.name && campos.email && campos.pass && terminos.checked
+}
+console.log(validate())
+console.log(campos)
+
+
